6fefb3bc96f79ad8f5b516b6d7e75c84ca4f7686
Harley Swift <114499670+ytnskc95@users.noreply.github.com>
2026-04-02 20:55:45 +0800

    Update AncientPredictorMod.cs

--- AncientPredictorMod.cs
+++ AncientPredictorMod.cs
@@ -5,6 +5,7 @@ using Godot;
 using HarmonyLib;
 using AncientPredictor.UI;
 using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Modding;
 using MegaCrit.Sts2.Core.Nodes;
 using MegaCrit.Sts2.Core.Nodes.Screens.Map;
 using MegaCrit.Sts2.Core.Runs;
